9fc5d4512f5840b43674762979c2a73747c6432f
jaydebaard <anton.sanjaya0889@gmail.com>
2025-01-15 23:28:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,7 +111,7 @@ if st.button("Generate Code"):
 # Input to decipher code
 cipher = st.text_input(
     "Enter Cipher Code to Decipher:",
-    placeholder="Enter cipher code (e.g., #Alpha#Sbtp-Efo#{Sigm})",
+    placeholder="Enter generated code (e.g., #Alpha#Sbtp-Efo#cima)",
 )
 
 if st.button("Decipher Code"):
